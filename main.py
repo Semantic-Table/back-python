ad2d47580c9d48878c02efbb1ab4fcfdf04f5a65
Marvin Le Peillet <marvin.lepeillet@gmail.com>
2025-03-31 15:36:31 +0200

    begin

---
+++ main.py
@@ -0,0 +1,40 @@
+# import fastapi and uvuicorn
+from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+
+app = FastAPI()
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
